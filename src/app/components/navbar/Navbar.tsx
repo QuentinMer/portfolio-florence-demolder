e3c32fd7f5cf9aed9146e8d5a801aa7ac775e11e
Quentin Mercier <153526325+QuentinMer@users.noreply.github.com>
2025-05-20 10:14:23 +0200

    Build ready for deployment

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -8,8 +8,16 @@ const Navbar = () => {
 
   return (
     <div className="font-cormorant font-semibold">
-      <ul className="mx-2 my-5 flex flex-row justify-between items-center">
-        <li className="">
+      <ul className="mx-2 my-5 flex flex-row justify-center md:justify-between items-center">
+        <li className="hidden md:block">
+          <Link 
+            className={`flex flex-row items-center gap-3 ${pathname === "/" ? "font-semibold italic underline" : ""}`} 
+            href="/"
+          >
+           <span>Accueil</span>
+          </Link>
+        </li>
+        <li className="absolute top-1  md:hidden">
           <Link 
             className={`flex flex-row items-center gap-3 ${pathname === "/" ? "font-semibold italic underline" : ""}`} 
             href="/"
@@ -18,7 +26,7 @@ const Navbar = () => {
           </Link>
         </li>
         <li>
-          <ul className="flex flex-row justify-end gap-4 ">
+          <ul className="flex flex-row justify-end gap-4 md:mt-0 mt-2">
             <li>
               <Link 
                 className={pathname === "/apropos" ? "font-semibold italic underline" : ""} 
